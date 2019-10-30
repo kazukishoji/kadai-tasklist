866480a459ed2ab17c9308a5418260356532f8da
keykun <ka.zucca33@gmail.com>
2019-10-30 09:31:11 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
+
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/delete'
     root to: "tasks#index"
+    
+    get "login", to: "sessions#new"
+    post "login", to: "sessions#create"
+    delete "logout", to: "sessions#destroy"
+    
+    get "signup", to: "users#new"
+    
     resources :tasks
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :users, only: [:index, :show, :new, :create]
 end
